0ca61afcd649685fba959ae63c49f66128b29f74
Siamion Karasik <senich2011@gmail.com>
2021-04-26 10:12:40 +0300

    use list.span in Task03

--- src/main/scala/scala2021/skarasik/task03/Task03.scala
+++ src/main/scala/scala2021/skarasik/task03/Task03.scala
@@ -1,22 +1,25 @@
 package scala2021.skarasik.task03
 
+import scala.annotation.tailrec
+
 object Task03 {
   def main(args: Array[String]): Unit = {
     val input = "aaaabccaadeeeee"
-    val output = encodeDirect(input)
+    val output = encodeDirect(input.toList)
     println(output)
   }
 
-  def encodeDirect(data: Iterable[Char]): List[(Int, Char)] = {
-    def encodeStep(encodedData: Seq[(Int, Char)], symbol: Char): Seq[(Int, Char)] = {
-      if (encodedData.isEmpty || encodedData.last._2 != symbol) {
-        encodedData.appended((1, symbol))
+  def encodeDirect(data: List[Char]): List[(Int, Char)] = {
+    @tailrec
+    def encodeStep(source: List[Char], encoded: List[(Int, Char)]): List[(Int, Char)] = {
+      if (source == Nil) {
+        encoded
       } else {
-        // might be memory inefficient?
-        encodedData.init.appended((encodedData.last._1 + 1, symbol))
+        val (sameValues, nextSource) = source.span(_ == source.head)
+        encodeStep(nextSource, encoded:::List((sameValues.length, sameValues.head)))
       }
     }
 
-    data.foldLeft(Seq[(Int, Char)]())(encodeStep).toList
+    encodeStep(data, List[(Int, Char)]())
   }
 }
