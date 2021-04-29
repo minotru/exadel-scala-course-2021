9bec4511e4eed148ff1a66920d3707cc9f536dc5
Siamion Karasik <senich2011@gmail.com>
2021-04-30 00:44:49 +0300

    added comments to Task04

--- src/main/scala/scala2021/skarasik/task04/Task04.scala
+++ src/main/scala/scala2021/skarasik/task04/Task04.scala
@@ -5,31 +5,56 @@ import scala.annotation.tailrec
 object Task04 {
   def main(args: Array[String]): Unit = {
     val testCases = Seq(
-      (List(2, 4, 6), 8, true),
-      (List(2, 4 ,6), 5, false)
+      (Seq(2, 4, 6), 8, true),
+      (Seq(2, 4 ,6), 5, false)
     )
 
+    println("Brute force solution")
     for (testCase <- testCases) {
       val (coins, change, expected) = testCase
       assert(canGiveChangeBruteForce(coins, change) == expected,
         s"Failed case: ${testCase}")
     }
+    println("All tests are passed.")
 
+    println("Recursive solution")
+    for (testCase <- testCases) {
+      val (coins, change, expected) = testCase
+      assert(canGiveChangeRecursive(coins, change) == expected,
+        s"Failed case: ${testCase}")
+    }
     println("All tests are passed.")
   }
 
-  def canGiveChangeBruteForce(coins: List[Int], change: Int): Boolean = {
+  def canGiveChangeRecursive(coins: Seq[Int], change: Int): Boolean = {
+    // this is a pure recursive solution (dynamic programming)
+    // but it is very ineffective.
+    // we need to maintain cache for already precomputed values Q(i, s)
+    // but cache needs to be shared across calls and to be a **mutable** structure.
+    // whereas we avoid mutable structures.
+    def Q(i: Int, s: Int): Boolean = {
+      if (i == 0) {
+        coins(i) == s
+      } else {
+        (coins(i) == s) || Q(i - 1, s) || Q(i - 1, s - coins(i))
+      }
+    }
+
+    Q(coins.length - 1, change)
+  }
+
+  def canGiveChangeBruteForce(coins: Seq[Int], change: Int): Boolean = {
     // a brute force solution - generate all subsets (2^N) an check the sum.
     // works with 32 coins at most (there are 32 in bits in Int).
     
     @tailrec
-    def _canGiveChangeBruteForce(coins: List[Int], change: Int, mask: Int): Boolean = {
-      val sum = mask
-        .toBinaryString
-        .take(coins.length)
-        .map(digit => if (digit == '1') 1 else 0)
-        .zip(coins)
-        .foldLeft(0)((s, p) => s + p._1 * p._2)
+    def _canGiveChangeBruteForce(coins: Seq[Int], change: Int, mask: Int): Boolean = {
+      val sum = coins
+        .zipWithIndex
+        .foldLeft(0)((sum, p) => {
+          val (coin, i) = p
+          if (((1 << i) & mask) > 0) sum + coin else sum
+        })
 
       if (sum == change) {
         true
